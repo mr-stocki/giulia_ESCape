306cc517242b6c1386704c05458ed721385d1a60
MageCoder <andre.schubert78@gmail.com>
2019-07-18 22:44:26 +0200

    update

--- ESCaped/ESCaped.ino
+++ ESCaped/ESCaped.ino
@@ -5,8 +5,8 @@
 
 
 //CAN-CH bus ID definitions
-#define TCESC_CONTROL       0x384
-#define SUSPENSION_CONTROL  0x1FC
+#define TCESC_CONTROL       0x384 // 900
+#define SUSPENSION_CONTROL  0x1FC // 508
 
 //CAN-CH bus data definitions
 #define SUSP_SOFT           0x50
@@ -14,11 +14,10 @@
 #define SUSP_FIRM           0x60
 
 //DNA Mode definitions
-#define DNA_RACE                  0x31
-#define DNA_DYNAMIC               0x9
-#define DNA_NEUTRAL               0x1
-#define DNA_ADVANCED_EFFICENCY_QV 0x11
-#define DNA_ADVANCED_EFFICENCY_TB 0x17
+#define DNA_RACE                  0x31 // 49
+#define DNA_DYNAMIC               0x9 // 9
+#define DNA_NEUTRAL               0x1 // 1
+#define DNA_ADVANCED_EFFICENCY    0x11 // 17
 
 #define CAN_5KBPS    1
 #define CAN_10KBPS   2
@@ -45,10 +44,6 @@ MCP_CAN CAN(SPI_CS_CAN);
 byte len = 0;
 byte buf[8] = {0};
 byte tcesc_buf[8] = {0};
-
-byte last_dna_mode = 0;
-byte left_stalk_count = 0;
-byte tc_disable = 0;
 
 void print_can_buf(int id, unsigned char len) {
     Serial.print(id, HEX);
@@ -80,12 +75,21 @@ void setup()
 }
 
 void handle_tcesc_control() {
-  if(tcesc_buf[1] != DNA_NEUTRAL && tcesc_buf[1] != DNA_DYNAMIC && tcesc_buf[1] != DNA_ADVANCED_EFFICENCY_TB && tcesc_buf[1] != DNA_RACE)
-    return;//sanity check, don't want to send random shit to module if buffer corrupt/incorrect
-  else if (tcesc_buf[1] == DNA_DYNAMIC) {
-    Serial.println("SEND MESSAGE TO TCESC control!");
-    CAN.sendMsgBuf(TCESC_CONTROL,0, 8, tcesc_buf);
+  if(tcesc_buf[1] != DNA_NEUTRAL && 
+      tcesc_buf[1] != DNA_DYNAMIC && 
+      tcesc_buf[1] != DNA_ADVANCED_EFFICENCY && 
+      tcesc_buf[1] != DNA_RACE) {
+    Serial.println("RETURN - DO NTH.");
+    Serial.println(tcesc_buf[1]);
+    return;
+  } else if (tcesc_buf[1] == DNA_DYNAMIC) {
+    Serial.println("buffer equals DNA_DYNAMIC, set to DNA_RACE");
+    tcesc_buf[1] = DNA_RACE;
   }
+
+  Serial.println("SEND MESSAGE TO TCESC control!");
+  Serial.println(tcesc_buf[1]);
+  CAN.sendMsgBuf(TCESC_CONTROL,0, 8, tcesc_buf);
 }
 
 
@@ -98,49 +102,13 @@ void loop()
     Serial.println("Got id from buffer: ");
     Serial.println(id);
     
-    
     if(id == TCESC_CONTROL) {
-    // if (id == DNA_DYNAMIC) {
+      Serial.println("ID seems to be TCESC_CONTROL");
       memcpy(tcesc_buf, buf, 8);
-
-      Serial.println("print buffer 1: ");
-      Serial.println(tcesc_buf[1]);
-      
-      Serial.println("print buffer 3: ");
-      Serial.println(tcesc_buf[3]);
-
-      // TODO: need to use another switch for example paddles!
-      //if(tcesc_buf[3] & 0x40) {//driver pressing left stalk button
-      if(tcesc_buf[3] == 8) {
-        left_stalk_count++;
-        if(left_stalk_count > 8) {
-          tc_disable ^= 1; //toggle tc_disable between 0 and 1
-          left_stalk_count = 0;//reset stalk count for next press event
-          Serial.println("tc_disable: ");
-          Serial.println(tc_disable);
-        }
-      }
-      else
-        left_stalk_count = 0;
-      /*
-       * let's detect real DNA mode change to/from race for QV cars and change to expected TC/ESC setting when that happens
-       */
-      if (last_dna_mode == DNA_DYNAMIC && tcesc_buf[1] != DNA_DYNAMIC) {
-        Serial.println("TCESC enabled");
-        tc_disable = 0;
-      } else if (last_dna_mode != DNA_DYNAMIC && tcesc_buf[1] == DNA_DYNAMIC) {
-        Serial.println("TCESC disabled");
-        tc_disable = 1;
-      }
-
-      Serial.println("Will set last DNA Mode: ");
-      Serial.println(tcesc_buf[1]);
-      last_dna_mode = tcesc_buf[1];
-    }
-    
-    //if (id == TCESC_CONTROL || id == SUSPENSION_CONTROL) {
-    if (id == TCESC_CONTROL) {
-      handle_tcesc_control();//added extra check to deal with library/filter bug, don't spam the bus
+      Serial.println("print buffer 1: ");   
+      Serial.println(tcesc_buf[1]);   
+           
+      handle_tcesc_control();
     }
   }
 }
