6efb053f8676fda5f150ce038613130dae1e8e32
MageCoder <andre.schubert78@gmail.com>
2019-07-18 21:38:23 +0200

    update

--- ESCaped/ESCaped.ino
+++ ESCaped/ESCaped.ino
@@ -14,10 +14,11 @@
 #define SUSP_FIRM           0x60
 
 //DNA Mode definitions
-#define DNA_RACE                0x31
-#define DNA_DYNAMIC             0x9
-#define DNA_NEUTRAL             0x1//might have swapped with A mode
-#define DNA_ADVANCED_EFFICENCY  0x11//might have swapped with N mode
+#define DNA_RACE                  0x31
+#define DNA_DYNAMIC               0x9
+#define DNA_NEUTRAL               0x1
+#define DNA_ADVANCED_EFFICENCY_QV 0x11
+#define DNA_ADVANCED_EFFICENCY_TB 0x17
 
 #define CAN_5KBPS    1
 #define CAN_10KBPS   2
@@ -75,19 +76,16 @@ void setup()
   CAN.init_Mask(1, 0, 0x7ff);//did you RTFM?
 
   CAN.init_Filt(0, 0, TCESC_CONTROL);
-  // CAN.init_Filt(1, 0, SUSPENSION_CONTROL);
+  CAN.init_Filt(1, 0, SUSPENSION_CONTROL);
 }
 
 void handle_tcesc_control() {
-  if(tcesc_buf[1] != DNA_NEUTRAL && tcesc_buf[1] != DNA_DYNAMIC && tcesc_buf[1] != DNA_ADVANCED_EFFICENCY && tcesc_buf[1] != DNA_RACE)
+  if(tcesc_buf[1] != DNA_NEUTRAL && tcesc_buf[1] != DNA_DYNAMIC && tcesc_buf[1] != DNA_ADVANCED_EFFICENCY_TB && tcesc_buf[1] != DNA_RACE)
     return;//sanity check, don't want to send random shit to module if buffer corrupt/incorrect
-  if(tc_disable && tcesc_buf[1] != DNA_RACE)
-    tcesc_buf[1] = DNA_RACE;//TCESC disable from Giulia QV in race mode
-  else if(!tc_disable && tcesc_buf[1] == DNA_RACE)
-    tcesc_buf[1] = DNA_DYNAMIC;//car in race mode wants TC, put TCESC in Dynamic mode
-  else
-    return;//nothing to do if driver wants TCESC on in non-race mode or off in race-mode.  Car can manage itself
-  CAN.sendMsgBuf(TCESC_CONTROL, 0, 8, tcesc_buf);
+  else if (tcesc_buf[1] == DNA_DYNAMIC) {
+    Serial.println("SEND MESSAGE TO TCESC control!");
+    CAN.sendMsgBuf(TCESC_CONTROL,0, 8, tcesc_buf);
+  }
 }
 
 
@@ -97,13 +95,29 @@ void loop()
   if (CAN.checkReceive() == CAN_MSGAVAIL) {//consider switch to interrupt mode
                                           
     CAN.readMsgBufID(&id, &len, buf);
+    Serial.println("Got id from buffer: ");
+    Serial.println(id);
+    
+    
     if(id == TCESC_CONTROL) {
+    // if (id == DNA_DYNAMIC) {
       memcpy(tcesc_buf, buf, 8);
-      if(tcesc_buf[3] & 0x40) {//driver pressing left stalk button
+
+      Serial.println("print buffer 1: ");
+      Serial.println(tcesc_buf[1]);
+      
+      Serial.println("print buffer 3: ");
+      Serial.println(tcesc_buf[3]);
+
+      // TODO: need to use another switch for example paddles!
+      //if(tcesc_buf[3] & 0x40) {//driver pressing left stalk button
+      if(tcesc_buf[3] == 8) {
         left_stalk_count++;
         if(left_stalk_count > 8) {
           tc_disable ^= 1; //toggle tc_disable between 0 and 1
           left_stalk_count = 0;//reset stalk count for next press event
+          Serial.println("tc_disable: ");
+          Serial.println(tc_disable);
         }
       }
       else
