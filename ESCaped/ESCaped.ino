f54cd6c15a878d26e416ad5bf45ad0c92b4c9ccc
MageCoder <andre.schubert78@gmail.com>
2019-07-19 13:16:50 +0200

    code cleanup

--- ESCaped/ESCaped.ino
+++ ESCaped/ESCaped.ino
@@ -19,25 +19,6 @@
 #define DNA_NEUTRAL               0x1 // 1
 #define DNA_ADVANCED_EFFICENCY    0x11 // 17
 
-#define CAN_5KBPS    1
-#define CAN_10KBPS   2
-#define CAN_20KBPS   3
-#define CAN_25KBPS   4
-#define CAN_31K25BPS 5
-#define CAN_33KBPS   6
-#define CAN_40KBPS   7
-#define CAN_50KBPS   8
-#define CAN_80KBPS   9
-#define CAN_83K3BPS  10
-#define CAN_95KBPS   11
-#define CAN_100KBPS  12
-#define CAN_125KBPS  13
-#define CAN_200KBPS  14
-#define CAN_250KBPS  15
-#define CAN_500KBPS  16
-#define CAN_666kbps  17
-#define CAN_1000KBPS 18
-
 #define SPI_CS_CAN  9
 MCP_CAN CAN(SPI_CS_CAN);     
 
@@ -45,18 +26,6 @@ byte len = 0;
 byte buf[8] = {0};
 byte tcesc_buf[8] = {0};
 byte last_dna_mode = DNA_NEUTRAL;
-
-void print_can_buf(int id, unsigned char len) {
-    Serial.print(id, HEX);
-    Serial.print(",");
-    Serial.print(len);
-    Serial.print(",");
-    for(int i = 0; i < len; i++) {
-      Serial.print(buf[i], HEX);
-      Serial.print(",");
-    }
-    Serial.println();
-}
 
 void setup()
 {
@@ -121,8 +90,7 @@ void convertBufToStr(byte string) {
 void loop()
 {
   unsigned long id = 0;
-  if (CAN.checkReceive() == CAN_MSGAVAIL) {//consider switch to interrupt mode
-                                          
+  if (CAN.checkReceive() == CAN_MSGAVAIL) {//consider switch to interrupt mode                
     CAN.readMsgBufID(&id, &len, buf);
     memcpy(tcesc_buf, buf, 8);
     
